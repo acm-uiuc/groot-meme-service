f72cfd4c54be399774d6f579a94963fda3d40a61
Duke Vijitbenjaronk <warut.vijitbenjaronk@gmail.com>
2016-12-29 15:34:17 -0600

    removed unnecessary files

--- groot_meme_service/meme_service/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class MemeServiceConfig(AppConfig):
-    name = 'meme_service'
--- groot_meme_service/meme_service/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^browse$', views.browse),
-    url(r'^query$', views.query),
-    url(r'^upload$', views.upload)
-]
