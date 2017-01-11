ef7381ea0223851ffb6103fd159ee37f8a780107
Benjamin Congdon <bencongdon96@gmail.com>
2017-01-10 21:16:44 -0800

    Flask-restful rewrite

--- groot_meme_service/app.py
+++ groot_meme_service/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models import db, Meme
 from settings import mysql
-from flask_restful import reqparse
+from flask_restful import Resource, Api, reqparse, inputs
 from sqlalchemy.sql.expression import func
 
 app = Flask(__name__)
@@ -12,57 +12,62 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/{}'.format(
     mysql['host'],
     mysql['dbname']
 )
+api = Api(app)
 
 
-@app.route("/memes", methods=['GET'])
-def get_memes():
-    ''' Endpoint for viewing memes '''
-    order_funcs = {
-        'random': func.random(),
-        'latest': Meme.uploaded_at
-    }
-    parser = reqparse.RequestParser()
-    parser.add_argument('order', choices=order_funcs.keys(), default='latest')
-    parser.add_argument('user')
-    args = parser.parse_args()
+class MemeListResource(Resource):
+    def get(self):
+        ''' Endpoint for viewing memes '''
+        order_funcs = {
+            'random': func.random(),
+            'latest': Meme.uploaded_at
+        }
+        parser = reqparse.RequestParser()
+        parser.add_argument('order', choices=order_funcs.keys(),
+                            default='latest')
+        parser.add_argument('netid')
+        args = parser.parse_args()
 
-    memes = db.session.query(Meme).order_by(order_funcs[args.get('order')])
+        memes = db.session.query(Meme).order_by(order_funcs[args.get('order')])
 
-    if 'user' in request.args:
-        memes = memes.filter_by(netid=request.args['user'])
+        if args.netid:
+            memes = memes.filter_by(netid=args.netid)
 
-    memes = memes.limit(25)
+        memes = memes.limit(25)
 
-    return jsonify([m.to_dict() for m in memes])
+        return jsonify([m.to_dict() for m in memes])
 
+    def post(self):
+        ''' Endpoint for uploading memes '''
+        parser = reqparse.RequestParser()
+        parser.add_argument('url', required=True,
+                            type=inputs.regex('https?:\/\/.*\.(?:png|jpg)'))
+        parser.add_argument('title')
+        parser.add_argument('netid')
+        args = parser.parse_args()
 
-@app.route("/memes/<int:meme_id>", methods=['GET'])
-def get_meme(meme_id):
-    meme = db.session.query(Meme).filter_by(id=meme_id).first()
-    print meme
-    if meme:
+        meme = Meme(
+            url=args.url,
+            title=args.title,
+            netid=args.netid
+        )
+        db.session.add(meme)
+        db.session.commit()
         return jsonify(meme.to_dict())
-    else:
-        return "No meme with id %s" % meme_id, 404
 
 
-@app.route("/memes", methods=['POST'])
-def upload():
-    ''' Endpoint for uploading memes '''
-    parser = reqparse.RequestParser()
-    parser.add_argument('url', required=True)
-    parser.add_argument('title')
-    parser.add_argument('url')
-    args = parser.parse_args()
+class MemeResource(Resource):
+    def get(self, meme_id):
+        meme = db.session.query(Meme).filter_by(id=meme_id).first()
+        print meme
+        if meme:
+            return jsonify(meme.to_dict())
+        else:
+            return "No meme with id %s" % meme_id, 404
 
-    meme = Meme(
-        url=args.url,
-        title=args.title,
-        netid=args.netid
-    )
-    db.session.add(meme)
-    db.session.commit()
-    return jsonify(meme.to_dict())
+api.add_resource(MemeResource, '/memes/<int:meme_id>', endpoint='meme')
+api.add_resource(MemeListResource, '/memes', endpoint='memes')
+
 
 if __name__ == "__main__":
     db.init_app(app)
