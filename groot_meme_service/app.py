4f28ca81741eaa97baca7c1c29c1cdbc9becfe02
Benjamin Congdon <bencongdon96@gmail.com>
2017-01-12 13:12:46 -0800

    Licensing, clarifying key in meme dict

--- groot_meme_service/app.py
+++ groot_meme_service/app.py
@@ -1,3 +1,11 @@
+'''
+Copyright © 2016, ACM@UIUC
+This file is part of the Groot Project.
+The Groot Project is open source software, released under the University of
+Illinois/NCSA Open Source License.  You should have received a copy of
+this license in a file with the distribution.
+'''
+
 from flask import Flask, jsonify
 import os
 import requests
@@ -104,7 +112,7 @@ class MemeListResource(Resource):
         return jsonify({
             'memes': memes_dict,
             'page': args.page,
-            'num_pages': page.pages 
+            'num_pages': page.pages
         })
 
     def post(self):
--- groot_meme_service/models.py
+++ groot_meme_service/models.py
@@ -1,3 +1,11 @@
+'''
+Copyright © 2016, ACM@UIUC
+This file is part of the Groot Project.
+The Groot Project is open source software, released under the University of
+Illinois/NCSA Open Source License.  You should have received a copy of
+this license in a file with the distribution.
+'''
+
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
@@ -19,7 +27,7 @@ class Meme(db.Model):
             'url': self.url,
             'created_at': self.created_at.isoformat(),
             'title': self.title,
-            'netid': self.netid,
+            'author': self.netid,
             'votes': self.votes.count()
         }
 
